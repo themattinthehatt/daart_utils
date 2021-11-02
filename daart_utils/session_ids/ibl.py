68b839883570138005cedb1d669c0d80414c5ad9
themattinthehatt <themattinthehatt@gmail.com>
2021-11-02 10:09:57 -0400

    add oft data info

--- daart_utils/session_ids/ibl.py
+++ daart_utils/session_ids/ibl.py
@@ -4,6 +4,7 @@ label_names = ['background', 'still', 'move', 'wheel_turn', 'groom']
 
 # test ids that are not used for testing
 SESS_IDS_TEST = [
+    'churchlandlab_CSHL045_2020-02-27-001',
     'cortexlab_KS020_2020-02-06-001',
     'hoferlab_SWC_043_2020-09-15-001',
     'mrsicflogellab_SWC_052_2020-10-22-001',
@@ -19,6 +20,7 @@ SESS_IDS_TRAIN_5 = [[
 ]]
 
 SESS_IDS_ALL = [
+    'churchlandlab_CSHL045_2020-02-27-001',
     'cortexlab_KS020_2020-02-06-001',
     'danlab_DY_009_2020-02-27-001',
     'danlab_DY_018_2020-10-15-001',
---
+++ daart_utils/session_ids/oft.py
@@ -0,0 +1,39 @@
+"""Hard-coded session combinations for training/testing daart models on mouse oft data."""
+
+label_names = ['background', 'supported', 'unsupported', 'grooming']
+
+# test ids that are not used for testing
+SESS_IDS_TEST = [
+
+]
+
+SESS_IDS_TRAIN_5 = [[  # choose sessions with most grooming (can be highly variable)
+    'OFT_16',
+    'OFT_38',
+    'OFT_39',
+    'OFT_52',
+    'OFT_58',
+]]
+
+SESS_IDS_ALL = [
+    'OFT_5',
+    'OFT_6',
+    'OFT_11',
+    'OFT_12',
+    'OFT_14',
+    'OFT_15',
+    'OFT_16',
+    'OFT_23',
+    'OFT_24',
+    'OFT_38',
+    'OFT_39',
+    'OFT_41',
+    'OFT_43',
+    'OFT_44',
+    'OFT_49',
+    'OFT_50',
+    'OFT_51',
+    'OFT_52',
+    'OFT_54',
+    'OFT_58',
+]

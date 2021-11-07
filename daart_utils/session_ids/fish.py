64c1af6f37686f3ef618cb7be354b0e214ef5642
themattinthehatt <themattinthehatt@gmail.com>
2021-11-07 12:14:31 -0500

    add new datasets

---
+++ daart_utils/session_ids/fish.py
@@ -0,0 +1,38 @@
+"""Hard-coded session combinations for training/testing daart models on fish data."""
+
+label_names = ['flaring', 'tailbeating']
+
+# test ids that are not used for testing
+SESS_IDS_TEST = [
+]
+
+SESS_IDS_TRAIN_10 = [[  # choose sessions with most grooming (can be highly variable)
+
+]]
+
+SESS_IDS_ALL = [
+    '1.1.28_L',
+    '1.1.28_R',
+    '1.2.28_L',
+    '1.2.28_R',
+    '2.1.28_L',
+    '2.1.28_R',
+    '2.2.28_L',
+    '2.2.28_R',
+    '3.1.28_L',
+    '3.1.28_R',
+    '3.2.28_L',
+    '3.2.28_R',
+    '4.1.28_L',
+    '4.1.28_R',
+    '4.2.28_L',
+    '4.2.28_R',
+    'IM1_IM2_2.1.1_L',
+    'IM1_IM2_2.1.1_R',
+    'IM2_IM4_5.1.2_L',
+    'IM2_IM4_5.1.2_R',
+    'VM1_VM3_1.1.1_L',
+    'VM1_VM3_1.1.1_R',
+    'VM3_VM4_5.1.1_L',
+    'VM3_VM4_5.1.1_R',
+]
---
+++ daart_utils/session_ids/resident_intruder.py
@@ -0,0 +1,29 @@
+"""Hard-coded session combinations for training/testing daart models on resident-intruder data."""
+
+label_names = ['TODO']
+
+# test ids that are not used for testing
+SESS_IDS_TEST = [
+]
+
+SESS_IDS_TRAIN_10 = [[  # choose sessions with most grooming (can be highly variable)
+
+]]
+
+SESS_IDS_ALL = [
+    'exp24_mouse2_day2',
+    'exp24_mouse2_day4',
+    'exp24_mouse2_day5',
+    'exp25_mouse1_day6',
+    'exp25_mouse2_day2',
+    'exp25_mouse2_day6',
+    # w/o hand labels
+    'exp24_mouse2_day1',
+    'exp24_mouse2_day6',
+    'exp25_mouse2_day3',
+    'exp25_mouse2_day4',
+    'exp32_mouse1_day1',
+    'exp32_mouse2_day1',
+    'exp33_mouse1_day4',
+    'exp33_mouse1_day5',
+]

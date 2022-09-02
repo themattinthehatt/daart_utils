4f83de5b4e878a4f3c868ff0bc2a96f65a8bd138
themattinthehatt <themattinthehatt@gmail.com>
2022-09-02 10:46:05 -0400

    finalizing app updates

--- apps/diagnostics.py
+++ apps/diagnostics.py
@@ -49,11 +49,15 @@ if 'handler' not in st.session_state:
 if 'fig_traces' not in st.session_state:
     st.session_state.fig_traces = None
 if 'idx' not in st.session_state:
-    st.session_state.idx = 0
+    st.session_state.idx = int(0)
 if 'window' not in st.session_state:
     st.session_state.window = int(200)
 if 'frame_skip' not in st.session_state:
-    st.session_state.frame_skip = int(1)
+    st.session_state.frame_skip = int(10)
+if 'include_video' not in st.session_state:
+    st.session_state.include_video = False
+if 'update_plot' not in st.session_state:
+    st.session_state.update_plot = False
 
 
 def st_directory_picker(initial_path=Path()):
@@ -70,8 +74,8 @@ def st_directory_picker(initial_path=Path()):
     _, col1, col2, col3, _ = st.columns([3, 1, 3, 1, 3])
 
     with col1:
-        st.markdown("#")
-        if st.button("⬅️") and "path" in st.session_state:
+        st.markdown('#')
+        if st.button('⬅️', key='dir_back') and 'path' in st.session_state:
             st.session_state.path = st.session_state.path.parent
             st.experimental_rerun()
 
@@ -84,7 +88,7 @@ def st_directory_picker(initial_path=Path()):
         ]
         if subdirectroies:
             st.session_state.new_dir = st.selectbox(
-                "Subdirectories", sorted(subdirectroies)
+                'Subdirectories', sorted(subdirectroies)
             )
         else:
             st.markdown("#")
@@ -94,8 +98,8 @@ def st_directory_picker(initial_path=Path()):
 
     with col3:
         if subdirectroies:
-            st.markdown("#")
-            if st.button("➡️") and "path" in st.session_state:
+            st.markdown('#')
+            if st.button('➡️', key='dir_forward') and 'path' in st.session_state:
                 st.session_state.path = Path(
                     st.session_state.path, st.session_state.new_dir
                 )
@@ -183,16 +187,17 @@ def run():
 
         """)
 
-    # trace_dir = st.text_input(
-    #     'Directory name for traces (e.g. `markers` or `features`)',
-    #     value='features')
-    # data_dir = st_directory_picker()
+    trace_dir = st.text_input(
+        'Directory name for traces (e.g. `markers` or `features`)',
+        value='features')
+    model_labels_dir = 'labels-model'
+    data_dir = st_directory_picker()
 
-    trace_dir = 'features-aug'
-    data_dir = '/media/mattw/behavior/daart-data/fish/tmp'
+    # trace_dir = 'features-aug'
+    # data_dir = '/media/mattw/behavior/daart-data/fish/tmp'
 
     available_dirs = [d for d in os.listdir(data_dir) if os.path.isdir(os.path.join(data_dir, d))]
-    if trace_dir not in available_dirs:
+    if trace_dir not in available_dirs or model_labels_dir not in available_dirs:
         sess_ids = []
         st.warning('Current directory does not contain the correct subdirectories')
     else:
@@ -204,7 +209,6 @@ def run():
 
         load_data_submit = st.button('Load data')
         if load_data_submit:
-            print('load data')
             st.session_state.handler = load_data(sess_id, data_dir, trace_dir)
 
     # ---------------------------------------------------------------------------------------------
@@ -226,47 +230,65 @@ def run():
                 for f in st.session_state.handler.features.names:
                     include_feature[f] = st.checkbox(f, value=include_feature[f])
 
-            window_ = st.text_input('Plot width (frames)', st.session_state.window)
-            window_ = int(window_)
-            if st.session_state.window != window_:
-                st.session_state.window = window_
+            if st.session_state.include_video:
+                window_ = st.text_input('Plot width (frames)', st.session_state.window)
+                window_ = int(window_)
+                if st.session_state.window != window_:
+                    st.session_state.window = window_
 
-            st.form_submit_button('Update plot')
+            update_plot = st.form_submit_button('Update plot')
+            # signal update if True; if False, leave alone since an update might be triggered
+            # elsewhere
+            if update_plot:
+                st.session_state.update_plot = True
 
         # select video options
         if st.session_state.handler.video is not None:
             st.markdown('###### Video options')
 
             include_video = st.checkbox('Include video data')
+            if include_video != st.session_state.include_video:
+                st.session_state.update_plot = True  # update plot when adding/removing video
+                st.session_state.include_video = include_video
+                st.experimental_rerun()
 
-            if include_video:
+            if st.session_state.include_video:
                 skip_ = st.text_input('Arrow skip (frames)', st.session_state.frame_skip)
                 skip_ = int(skip_)
                 if st.session_state.frame_skip != skip_:
                     st.session_state.frame_skip = skip_
 
         else:
-            include_video = False
+            st.session_state.include_video = False
 
-    if st.session_state.handler is not None:
+    # text
+    if st.session_state.fig_traces is not None or st.session_state.update_plot:
         st.text('')
         st.text('')
         st.markdown('#### Data UI')
+        st.text('')
 
     # ---------------------------------------------------------------------------------------------
     # plot frames
     # ---------------------------------------------------------------------------------------------
-    if st.session_state.handler is not None and include_video:
+    if st.session_state.handler is not None and st.session_state.include_video:
 
         min_frames = 0
         max_frames = int(st.session_state.handler.video.n_frames - 1)
 
         # select frame index
-        col1, col2, col3 = st.columns([1, 8, 1])
+        col0, col1, col2, col3, col4 = st.columns([1, 1, 6, 1, 1])
+        with col0:
+            st.markdown('#')
+            if st.button('️⬅️⬅️', key='frame_back_n'):
+                st.session_state.idx -= st.session_state.frame_skip
+                st.session_state.update_plot = True
+                st.experimental_rerun()  # update all input elements
         with col1:
             st.markdown('#')
-            if st.button('️⬅️'):
-                st.session_state.idx -= st.session_state.frame_skip
+            if st.button('️⬅️', key='frame_back_1'):
+                st.session_state.idx -= 1
+                st.session_state.update_plot = True
                 st.experimental_rerun()  # update all input elements
         with col2:
             idx_ = st.slider(
@@ -276,11 +298,19 @@ def run():
             )
             if st.session_state.idx != int(idx_):
                 st.session_state.idx = int(idx_)
+                st.session_state.update_plot = True
                 st.experimental_rerun()  # update all input elements
         with col3:
             st.markdown('#')
-            if st.button('➡️'):
+            if st.button('➡️', key='frame_forward_1'):
+                st.session_state.idx += 1
+                st.session_state.update_plot = True
+                st.experimental_rerun()  # update all input elements
+        with col4:
+            st.markdown('#')
+            if st.button('➡️➡️', key='frame_forward_n'):
                 st.session_state.idx += st.session_state.frame_skip
+                st.session_state.update_plot = True
                 st.experimental_rerun()  # update all input elements
 
         # safeguard from accessing bad frames
@@ -306,44 +336,61 @@ def run():
     # ---------------------------------------------------------------------------------------------
     if st.session_state.handler is not None:
 
-        state_names = (st.session_state.handler.hand_labels.names
-                       or st.session_state.handler.model_labels.names)
+        if st.session_state.update_plot:
+            state_names = (st.session_state.handler.hand_labels.names
+                           or st.session_state.handler.model_labels.names)
 
-        # x-axis
-        if include_video:
-            x = np.arange(
-                int(st.session_state.idx - st.session_state.window),
-                int(st.session_state.idx + st.session_state.window),
+            # x-axis
+            if st.session_state.include_video:
+                x = np.arange(
+                    int(st.session_state.idx - st.session_state.window),
+                    min(
+                        int(st.session_state.idx + st.session_state.window),
+                        int(st.session_state.handler.video.n_frames - 1)),
+                )
+                add_vertical_line = st.session_state.idx
+            else:
+                x = np.arange(st.session_state.handler.model_labels.vals.shape[0])
+                add_vertical_line = None
+
+            # hand labels
+            if st.session_state.handler.hand_labels.vals is not None:
+                states_hand = st.session_state.handler.hand_labels.vals[x]
+                states_hand[states_hand != 1] = np.nan
+            else:
+                states_hand = None
+
+            # model outputs
+            states_ = np.argmax(st.session_state.handler.model_labels.vals[x], axis=1)
+            states = MakeOneHot(n_classes=len(state_names))(states_)
+            states[states != 1] = np.nan
+
+            state_probs = st.session_state.handler.model_labels.vals[x]
+            features = st.session_state.handler.features.vals[x]
+
+            # crop beginning if necessary
+            if st.session_state.include_video \
+                    and (st.session_state.idx - st.session_state.window) < 0:
+                d = st.session_state.window - st.session_state.idx
+                if states_hand is not None:
+                    states_hand[:d] = np.nan
+                states[:d] = np.nan
+                state_probs[:d] = np.nan
+                features[:d] = np.nan
+
+            fig_traces = plotly_markers_and_states(
+                x=x,
+                states_hand=states_hand,
+                states_model=states,
+                states_probs=state_probs,
+                state_names=state_names,
+                features=features,
+                include_feature=include_feature,
+                add_vertical_line=add_vertical_line,
             )
-            add_vertical_line = st.session_state.idx
-        else:
-            x = np.arange(st.session_state.handler.model_labels.vals.shape[0])
-            add_vertical_line = None
 
-        # hand labels
-        if st.session_state.handler.hand_labels.vals is not None:
-            states_hand = st.session_state.handler.hand_labels.vals[x]
-            states_hand[states_hand != 1] = np.nan
-        else:
-            states_hand = None
-
-        # model outputs
-        states_ = np.argmax(st.session_state.handler.model_labels.vals[x], axis=1)
-        states = MakeOneHot(n_classes=len(state_names))(states_)
-        states[states != 1] = np.nan
-
-        fig_traces = plotly_markers_and_states(
-            x=x,
-            states_hand=states_hand,
-            states_model=states,
-            states_probs=st.session_state.handler.model_labels.vals[x],
-            state_names=state_names,
-            features=st.session_state.handler.features.vals[x],
-            include_feature=include_feature,
-            add_vertical_line=add_vertical_line,
-        )
-
-        st.session_state.fig_traces = fig_traces
+            st.session_state.fig_traces = fig_traces
+            st.session_state.update_plot = False
 
         if st.session_state.fig_traces is not None:
             # st.session_state.fig_traces.update_xaxes(
